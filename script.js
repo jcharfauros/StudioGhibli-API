f0090defc4521e66f64e7e6ef3353d533ec9c6b6
jcharfauros <julia.charfauros@gmail.com>
2021-07-11 22:39:21 -0400

    Update CSS

--- script.js
+++ script.js
@@ -14,9 +14,16 @@ const filmInfoBtn = document.querySelector('.btn');
 const modalBody = document.querySelector('.modal-body');
 const modalTitle = document.querySelector('.modal-title');
 
+
 fetch(baseURL)
 .then(response => response.json())
 .then(json => displayResults(json));
+
+
+function reloadFilm(){
+    reloadFilm = location.reload();
+}
+document.getElementById('randomBtn').addEventListener('click', reloadFilm);
 
 function displayResults (ghibliFilms){
 
@@ -27,19 +34,30 @@ function displayResults (ghibliFilms){
     let synopsis = document.createElement('p');
     let releaseYr = document.createElement('p');
     let filmOGtitle = document.createElement('h3');
+    let filmOGromTitle = document.createElement('h5');
     let director = document.createElement('h4');
-
+    let runTime = document.createElement('p');
+    
     filmTitle.innerText = g.title;
     filmOGtitle.innerText = g.original_title;
-    director.innerText = g.director;
+    filmOGromTitle.innerText = '('+g.original_title_romanised+')';
+    director.innerText = 'Film Director: ' + g.director;
     synopsis.innerText = g.description;
-    releaseYr.innerText = 'Year of release: ' + g.release_date; 
-    
+    releaseYr.innerText = 'Release: ' + g.release_date; 
+    runTime.innerText = 'Film runtime: ' + g.running_time + ' minutes';
     
     cardBody.insertBefore(filmTitle, filmInfoBtn);
     cardBody.insertBefore(filmOGtitle, filmInfoBtn);
+    cardBody.insertBefore(filmOGromTitle, filmInfoBtn);
     cardBody.insertBefore(releaseYr, filmInfoBtn);
 
     modalTitle.appendChild(director);
-    modalBody.appendChild(synopsis);
+    modalBody.appendChild(runTime);
+    modalBody.appendChild(synopsis);    
+}
+
+function displayYTvideo (filmTrailers) {
+    let filmTrailer = {
+        
+    }
 }
